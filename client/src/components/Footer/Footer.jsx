451f24b8f920ed00d1ca64808c9bf9fda5ba44fc
Deniscj1988 <deniscj1988@gmail.com>
2023-11-05 20:12:22 +0000

    Revert "Revert "first commit""
    This reverts commit 6c66cfee389f55913f82880f276fae4ee6e00ee4.

--- client/src/components/Footer/Footer.jsx
+++ client/src/components/Footer/Footer.jsx
@@ -1,10 +1,10 @@
 import React, { useEffect, useState } from "react";
 import {
   FaFacebookSquare,
-  FaTwitter,
   FaInstagram,
   FaLinkedin,
 } from "react-icons/fa";
+import Twitter from "../../assets/img/social/twitter.svg"
 import Logo from "../../assets/img/logo.png";
 import "./Footer.css";
 
@@ -30,7 +30,7 @@ const Footer = () => {
       </div>
       <div className="footer__social">
         <a href="https://www.twitter.com" rel="noopener noreferrer">
-          <FaTwitter />
+         <img className="social" src={Twitter} alt="twitter logo" />
         </a>
         <a href="https://www.facebook.com" rel="noopener noreferrer">
           <FaFacebookSquare />
@@ -55,11 +55,11 @@ const Footer = () => {
       </div>
       {isMobile ? (
         <p className="footer__mobile">
-          Just type what's on your mind and we'll
+          &copy; {new Date().getFullYear()} mijusovs.lv
         </p>
       ) : (
         <div className="footer__desktop">
-          Copyright &copy; {new Date().getFullYear()} Bella Onojie.com
+          Copyright &copy; {new Date().getFullYear()} mijusovs.lv
         </div>
       )}
     </footer>
--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -7,29 +7,41 @@ import "./Header.css";
 const Header = () => {
   const menuItems = [
     { label: "Home", link: "/" },
-    { label: "Product", link: "/about" },
-    { label: "Faq", link: "/faq" },
+    { label: "About", link: "/about" },
+    { label: "Portfolio", link: "/portfolio" },
     { label: "Contact", link: "/contact" },
   ];
+  window.addEventListener("scroll", function () {
+    const header = document.querySelector(".header-content");
+    const scrollTop = window.scrollY;
+
+    if (scrollTop > 100) {
+      header.style.backgroundColor = "rgba(0, 0, 0, 0.9)"; // Change the background color when scrolled
+    } else {
+      header.style.backgroundColor = "rgba(0, 0, 0, 0.5)"; // Set it back to transparent
+    }
+  });
 
   return (
     <div className="header">
-      <div className="menu">
-        <BurgerMenu />
-      </div>
-      <nav className="navbar">
-        <Link to={"/"}>
-          <img className="logo" src={Logo} alt="Company Logo" />
-        </Link>
-
-        <div className="menu-items">
-          {menuItems.map((item) => (
-            <NavLink className="menu-item" key={item.label} to={item.link}>
-              {item.label}
-            </NavLink>
-          ))}
+      <div className="header-content">
+        <div className="menu">
+          <BurgerMenu />
         </div>
-      </nav>
+        <nav className="navbar">
+          <Link to={"/"}>
+            <img className="logo" src={Logo} alt="Company Logo" />
+          </Link>
+
+          <div className="menu-items">
+            {menuItems.map((item) => (
+              <NavLink className="menu-item" key={item.label} to={item.link}>
+                {item.label}
+              </NavLink>
+            ))}
+          </div>
+        </nav>
+      </div>
     </div>
   );
 };
